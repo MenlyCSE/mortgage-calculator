5f2021559b4f8b645429a2cc22235a5d48547ba9
MenlyCSE <MenlyCSE@gmail.com>
2024-08-07 13:52:47 -0400

    update

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,19 @@
 import java.util.Scanner;
 
+//        // currency: $1,234,567
+//        // slashes: 0.1 --> 10%
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Name: ");
+//        String name = scanner.nextLine().trim();
+//        System.out.println("You are " + name);
+
+
 public class Main {
     public static void main(String[] args) {
-        // currency: $1,234,567
-        // slashes: 0.1 --> 10%
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Name: ");
-        String name = scanner.nextLine().trim();
-        System.out.println("You are " + name);
+        // Principal (amount of loan)
+        // Interest rate (%)
+        // Period (Years)
+        // Prints Monthly payments for mortgage in currency
+
     }
 }
