2e59530b85d09aa61cf7769c927f6bbde9823bd1
MenlyCSE <MenlyCSE@gmail.com>
2024-08-09 13:10:53 -0400

    completion

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,35 @@
+import java.text.NumberFormat;
 import java.util.Scanner;
-
-//        // currency: $1,234,567
-//        // slashes: 0.1 --> 10%
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.print("Name: ");
-//        String name = scanner.nextLine().trim();
-//        System.out.println("You are " + name);
-
 
 public class Main {
     public static void main(String[] args) {
-        // Principal (amount of loan)
-        // Interest rate (%)
-        // Period (Years)
-        // Prints Monthly payments for mortgage in currency
+        final int monthsInYear = 12;
+        final int percent = 100;
 
-        Scanner amountLoan = new Scanner(System.in); // input
-        System.out.print("Amount of loan: "); // prompt
-        String amountLoanRes = amountLoan.nextLine().trim(); // remove spaces & returns input
-        System.out.println(amountLoanRes);
+        // input object
+        Scanner userInput = new Scanner(System.in);
+
+        // user input - principal
+        System.out.print("Principal: ");
+        int principal = userInput.nextInt();
+
+        // user input - annual interest rate
+        System.out.print("Annual Interest Rate: ");
+        float annualInterest = userInput.nextFloat();
+
+        // user input - amount of years (period)
+        System.out.print("Amount of years: ");
+        int amountOfYears = userInput.nextInt();
+
+        // calculating monthly interest, number of payments, and mortgage
+        float monthlyInterest = annualInterest / percent / monthsInYear;
+        int numberOfPayments = amountOfYears * monthsInYear;
+        double mortgage = principal *
+                (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments)
+                / Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+
+        // output
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("Mortgage: " + mortgageFormatted);
     }
 }
