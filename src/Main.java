b57bf1a6805e153830b9fef4cf72f4e2c60fd433
MenlyCSE <MenlyCSE@gmail.com>
2024-08-10 09:44:09 -0400

    input validations

--- src/Main.java
+++ src/Main.java
@@ -5,25 +5,52 @@ public class Main {
     public static void main(String[] args) {
         final int monthsInYear = 12;
         final int percent = 100;
-
-        // input object
+        int principal = 0;
+        int amountOfYears = 0;
+        int numberOfPayments = 0;
+        float annualInterest = 0;
+        float monthlyInterest = 0;
         Scanner userInput = new Scanner(System.in);
 
-        // user input - principal
-        System.out.print("Principal: ");
-        int principal = userInput.nextInt();
+        // loop - principal
+        while (true) {
+            System.out.print("Principal ($1K - $1M): ");
+            principal = userInput.nextInt();
 
-        // user input - annual interest rate
-        System.out.print("Annual Interest Rate: ");
-        float annualInterest = userInput.nextFloat();
+            if (principal >= 1_000 && principal <= 1_000_000)
+                break;
+
+            System.out.println("Please enter a number between $1 and $1,000,000...");
+        }
+
+        // loop - annual & monthly interest rate
+        while (true) {
+            System.out.print("Annual Interest Rate (1% - 30%): ");
+            annualInterest = userInput.nextFloat();
+
+            if (annualInterest >= 1 && annualInterest <= 30) {
+                monthlyInterest = annualInterest / percent / monthsInYear;
+                break;
+            }
+
+            System.out.println("Please enter a number between 1% and 30%...");
+        }
 
         // user input - amount of years (period)
-        System.out.print("Amount of years: ");
-        int amountOfYears = userInput.nextInt();
+        while (true) {
+            System.out.print("Amount of years (1 - 30): ");
+            amountOfYears = userInput.nextInt();
+
+            if (amountOfYears >= 1 && amountOfYears <= 30) {
+                numberOfPayments = amountOfYears * monthsInYear;
+                break;
+
+            }
+
+            System.out.println("Please enter a number between 1 and 30...");
+        }
 
         // calculating monthly interest, number of payments, and mortgage
-        float monthlyInterest = annualInterest / percent / monthsInYear;
-        int numberOfPayments = amountOfYears * monthsInYear;
         double mortgage = principal *
                 (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments)
                 / Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
